b811379e70d683b87f1c8795c7f09cd661113cbf
David Poindexter <dpoindexter@nvisionative.com>
2020-03-19 22:25:43 -0400

    Add .git to end of starter repo URLs for cloning

--- src/main.js
+++ src/main.js
@@ -21,8 +21,8 @@ async function gitClone(options) {
     let repo = (options.extensionType.toLowerCase() !== '*custom' ? 'https://github.com/DNNCommunity/starter-' + 
         options.extensionType.replace(' ', '-').toLowerCase() + 
         (options.moduleType !== undefined && options.extensionType.toLowerCase() === 'module' ? '-' + options.moduleType.replace(' ', '-').toLowerCase() : '') + 
-        (options.personaBarModuleType !== undefined && options.extensionType.toLowerCase() === 'persona bar' ? '-' + options.personaBarModuleType.replace(' ', '-').toLowerCase() : '') : options.customExtensionRepo) +
-        '.git';
+        (options.personaBarModuleType !== undefined && options.extensionType.toLowerCase() === 'persona bar' ? '-' + options.personaBarModuleType.replace(' ', '-').toLowerCase() : '') +
+        '.git' : options.customExtensionRepo);
 
     const result = await execa('git', ['clone', repo, '.'], {
         cwd: options.targetDirectory,
